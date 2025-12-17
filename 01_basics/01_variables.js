b8738d48b381bdf2d5d48eb17a459d26e854f186
Vijay Singh Diggi <105920990+codeivijay@users.noreply.github.com>
2025-12-17 08:59:12 +0000

    learned let, const and var: which should not be used ever

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,6 +2,14 @@ const accountId = 3393
 let accountEmail= "vijay"
 var accountPassword = "12345"
 accountCity = "Jaipur"
-
-accountId = 2
-console.log(accountId);
+let accountState;
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+//accountId = 2
+accountEmail="vcnaming@gmail.com"
+accountPassword ="we7783393"
+accountCity ="Jamshedpur"
+//console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
