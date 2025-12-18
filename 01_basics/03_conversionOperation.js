b9ba3c97bc84d48ccff3f0869cc1c439fe9c90c5
Vijay Singh Diggi <105920990+codeivijay@users.noreply.github.com>
2025-12-18 12:51:23 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc"
+
+//console.log(typeof score)
+//console.log(typeof (score))
+
+/* let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+console.log(typeof NaN)
+console.log(valueInNumber) */
+
+// "33" => easily gets converted to 33
+// "33abc" => NaN and also NaN typeof is also number
+// true=> 1 and false=> 0
+
+/* let isLoggedIn = "vijay"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn) */
+
+// 1=> true ; 0=> false
+// "" => false
+// "vijay" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
