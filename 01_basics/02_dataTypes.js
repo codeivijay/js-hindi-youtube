430a9dcc95762724c79122f08cfc1dbc221cc110
Vijay Singh Diggi <105920990+codeivijay@users.noreply.github.com>
2025-12-18 12:22:59 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as never version
+//alert( 3 + 3) // we are using node.js, not browser
+
+let name ="vijay"
+let aga = 33
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string 
+//boolean true or false
+// null - standalone value , null can be assigned too in a variable.
+// undefined - when variable is declared but but defined, means initialized a value
+// symbol => unique specially fully used in figma
+
+// object
+console.log(typeof undefined)
