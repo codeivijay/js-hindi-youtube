fd5b0ca6c2e6dec8410ab59e6adad93e34d01b38
Vijay Singh Diggi <105920990+codeivijay@users.noreply.github.com>
2025-12-18 13:06:59 +0000

    Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,8 +20,23 @@ console.log(booleanIsLoggedIn) */
 // "" => false
 // "vijay" => true
 
-let someNumber = 33
+/* let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+ */
+
+
+/* Operations  */
+
+let value = 3
+
+let negValue = -value
+//console.log(negValue)
+
+let str1 ="hello"
+let str2 = "vijay"
+
+let str3 = str1 + str2
+console.log(str3)
