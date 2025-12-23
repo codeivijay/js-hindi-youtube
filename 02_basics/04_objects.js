070308e3a66d33fc4f768edb0af6b761c7ffa217
Vijay Singh Diggi <105920990+codeivijay@users.noreply.github.com>
2025-12-23 16:03:17 +0000

    singleton, created object using constructor , access array object's properties, constructor object methods => Object.keys(), Object.values(), Object.entries() and hasOwnProperty

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,67 @@
+// singleton by constructor
+
+//const tinderUser = new Object() // singleton
+const tinderUser = {}
+
+tinderUser.id = "1234abc"
+tinderUser.name = "vinay"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser)
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullName: {
+        userFullName: {
+            firstName: "vijay",
+            lastName: "diggi"
+        }
+    }
+}
+
+// console.log(regularUser.fullName.userFullName);
+
+// console.log(regularUser.fullName?.userFullName.firstName)
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+const obj4 = {5: "e", 6: "f"}
+
+//const obj3 = {obj1, obj2}
+//console.log(obj3);
+//const obj3 = Object.assign({},obj1, obj2, obj4)
+const obj3 = {...obj1, ...obj2, ...obj4}
+//console.log(obj3)
+
+// when values comes from database
+
+const users = [
+    {
+        id: 1,
+        email: "vijay@gmail.com"
+    },
+    {
+        id:2,
+        email: "goman@gmail.com"
+    },
+   {
+        
+    },
+    {
+        
+    } 
+]
+
+users[1].email
+//console.log(users[1].email)
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.values(tinderUser).length)
+console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty('isLogged'));
+
+
+
