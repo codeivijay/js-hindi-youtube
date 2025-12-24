70642623afe1bd5049f1dcd50e155eb6139aaf3c
Vijay Singh Diggi <105920990+codeivijay@users.noreply.github.com>
2025-12-24 15:04:27 +0000

    when you declare a function and also hold the function in a variable is sometimes called expression, when you declare a function and also hold the function in a variable and then try to access that before that line it will throw error

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,13 +4,56 @@ let a = 300
 if(true) {
 let a = 10
 const b = 20
-console.log("Inner ", a)
+//console.log("Inner ", a)
 }
-for (let index = 0; index < array.length; index++) {
+/* for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
+} */
+
+
+//console.log(a)
+//console.log(b)
+
+// nested scopes
+
+function one() {
+    const username = "vijay"
+
+    function two() {
+        const website = "youtube"
+        console.log(username)
+    }
+    //console.log(website)
+    two()
+}
+
+//one()
+
+if (true) {
+    const username = "vijay"
+    if(username === "vijay") {
+        const website = "youtube"
+        //console.log(username + website)
+    }
+    //console.log(website)
+}
+
+//console.log(username)
+
+// ++++++++++++++++ interesting ++++++++++++
+console.log(addOne(5))
+
+function addOne(num) {
+    return num + 1
 }
 
 
-console.log(a)
-//console.log(b)
+
+// this is sometimes called expression, when you declare a function and also hold the function in a variable and then try to access that before that line it will throw error
+
+console.log(addTwo(5))
+const addTwo = function(num) {
+    return num + 2
+}
+
